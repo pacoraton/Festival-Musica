f1f8fb150394b9e75ec2138698125d324e1744fa
PacoRaton <pacocasino1@hotmail.com>
2021-04-23 13:36:59 -0500

    Se agrego la seccion de boletos, footer ademas de aplicar los efectos de scroll y navegacion fija

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,45 @@
-document.addEventListener("DOMContentLoaded", function(e){
-     hola();
+document.addEventListener("DOMContentLoaded", function(){
+        scrollNav();
+
+        navegacionFija();
     });
 
 
-    function hola(){
-        console.log("holajavascript.....");
+    function navegacionFija(){
+        const barra=document.querySelector('header');
+
+        //Registrar el Intersection Observer
+
+        const observer=new IntersectionObserver(function(entries){
+            if(entries[0].isIntersecting){
+                barra.classList.remove('fijo');
+            }else{
+                barra.classList.add('fijo');
+            }
+        });
+
+
+       //Elemento a observar
+       observer.observe( document.querySelector('.video'));     
+
+
+
     }
+
+//Dirigirnos a alguna parte de la pagina 
+function scrollNav(){
+    const enlaces = document.querySelectorAll('.navegacion-principal a');
+          
+          enlaces.forEach(function(enlace){
+            enlace.addEventListener('click',function(e){
+                e.preventDefault();
+
+                const seccion =document.querySelector(e.target.attributes.href.value);
+
+                seccion.scrollIntoView({
+                    behavior:'smooth'
+                });
+            });
+          })
+}
+    
